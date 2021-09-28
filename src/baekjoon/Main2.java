0b0080ef4a490182b5c20087f46866247fe58d98
김관민 <@DESKTOP-A5HFT58>
2021-09-28 14:21:48 +0900

    백준 문제 커밋연습 2

---
+++ src/baekjoon/Main2.java
@@ -0,0 +1,14 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class Main2{
+	public static void main(String args[]) {
+		Scanner scanner = new Scanner(System.in);
+		
+		int a = scanner.nextInt();
+		
+		System.out.println(a);
+	}
+}
+
