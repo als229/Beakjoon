ca354a9081921a37785a227f8b653138fb907181
김관민 <@DESKTOP-A5HFT58>
2021-09-29 21:49:45 +0900

    왜 안돼?

--- src/baekjoon/if2.java
+++ src/baekjoon/if2.java
@@ -35,30 +35,6 @@ package baekjoon;
 //		} else System.out.println("3");
 //	}
 //}
-import java.util.Scanner; 
-
-public class if2 {
-	public static void main(String args[]) {
-		Scanner scanner = new Scanner(System.in);
-
-		int a = scanner.nextInt();
-		int b = scanner.nextInt();
-		int c = 45;
-		b = b-c;
-		if(b <0) {
-			b = b+60;
-			a = a-1;
-		}
-		if(a <0) {
-			a= a+24;
-		}
-
-
-		System.out.println(a +" "+ b);
-	}
-}
-
-
 
 
 
--- src/baekjoon/ifpractice5.java
+++ src/baekjoon/ifpractice5.java
@@ -1,20 +1,37 @@
-package baekjoon;
 
-	import java.util.Scanner; 
+//import java.util.Scanner; * 정답
 
-	public class ifpractice5 {
-		public static void main(String args[]) {
-			Scanner scanner = new Scanner(System.in);
-			
-			int a = scanner.nextInt();
-			int b = scanner.nextInt();
-			
-			if (a >0 && b>0) {
-				System.out.println("1");
-			}else if (a>0 && b<0 ) {
-				System.out.println("4");
-			}else if (a <0 && b>0) {
-				System.out.println("2");
-			} else System.out.println("3");
-		}
-	}
+//public class if2{
+//	public static void main(String args[]) {
+//		Scanner scanner = new Scanner(System.in);
+//		
+//		int a = scanner.nextInt();
+//		
+//		if ((a %4==0 && a%100!=0) || a%400 ==0) {
+//			System.out.println("1");
+//		}else {
+//			System.out.println("0");
+//		}
+//	}
+//}
+
+//정답
+//import java.util.Scanner; 
+//
+//public class if2 {
+//	public static void main(String args[]) {
+//		Scanner scanner = new Scanner(System.in);
+//		
+//		int a = scanner.nextInt();
+//		int b = scanner.nextInt();
+//		
+//		if (a >0 && b>0) {
+//			System.out.println("1");
+//		}else if (a>0 && b<0 ) {
+//			System.out.println("4");
+//		}else if (a <0 && b>0) {
+//			System.out.println("2");
+//		} else System.out.println("3");
+//	}
+//}
+
