b0d5323e323f20f220602aa95acf360ee8179d36
김관민 <@DESKTOP-A5HFT58>
2021-09-29 20:13:01 +0900

    if 3번

---
+++ src/baekjoon/if2.java
@@ -0,0 +1,18 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class if2{
+	public static void main(String args[]) {
+		Scanner scanner = new Scanner(System.in);
+		
+		int a = scanner.nextInt();
+		
+		if ((a %4==0 && a%100!=0) || a%400 ==0) {
+			System.out.println("1");
+		}else {
+			System.out.println("0");
+		}
+	}
+}
+
