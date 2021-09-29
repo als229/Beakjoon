f0953a9c8fb7bd7bde7b2ae1f312efadf8205858
김관민 <@DESKTOP-A5HFT58>
2021-09-29 20:27:31 +0900

    if 4번 문제

---
+++ src/baekjoon/ifpractice4.java
@@ -0,0 +1,21 @@
+package baekjoon;
+
+	import java.util.Scanner; 
+
+	public class ifpractice4 {
+		public static void main(String args[]) {
+			Scanner scanner = new Scanner(System.in);
+			
+			int a = scanner.nextInt();
+			int b = scanner.nextInt();
+			
+			if (a >0 && b>0) {
+				System.out.println("1");
+			}else if (a>0 && b<0 ) {
+				System.out.println("4");
+			}else if (a <0 && b>0) {
+				System.out.println("2");
+			} else System.out.println("3");
+		}
+	}
+
