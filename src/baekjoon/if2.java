67589f237b901a8756d747ec3fae706f906c7c57
김관민 <@DESKTOP-A5HFT58>
2021-09-29 21:34:49 +0900

    이렇게 하는게 아닌가??

--- src/baekjoon/if2.java
+++ src/baekjoon/if2.java
@@ -17,24 +17,48 @@ package baekjoon;
 //}
 
 //Á¤´ä
+//import java.util.Scanner; 
+//
+//public class if2 {
+//	public static void main(String args[]) {
+//		Scanner scanner = new Scanner(System.in);
+//		
+//		int a = scanner.nextInt();
+//		int b = scanner.nextInt();
+//		
+//		if (a >0 && b>0) {
+//			System.out.println("1");
+//		}else if (a>0 && b<0 ) {
+//			System.out.println("4");
+//		}else if (a <0 && b>0) {
+//			System.out.println("2");
+//		} else System.out.println("3");
+//	}
+//}
 import java.util.Scanner; 
 
 public class if2 {
 	public static void main(String args[]) {
 		Scanner scanner = new Scanner(System.in);
-		
+
 		int a = scanner.nextInt();
 		int b = scanner.nextInt();
-		
-		if (a >0 && b>0) {
-			System.out.println("1");
-		}else if (a>0 && b<0 ) {
-			System.out.println("4");
-		}else if (a <0 && b>0) {
-			System.out.println("2");
-		} else System.out.println("3");
+		int c = 45;
+		b = b-c;
+		if(b <0) {
+			b = b+60;
+			a = a-1;
+		}
+		if(a <0) {
+			a= a+24;
+		}
+
+
+		System.out.println(a +" "+ b);
 	}
 }
+
+
 
 
 
