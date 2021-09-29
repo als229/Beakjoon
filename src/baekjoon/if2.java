dce1031b9465b903d74c19b4af4957689e072df0
김관민 <@DESKTOP-A5HFT58>
2021-09-29 20:24:54 +0900

    if 4번문제

--- src/baekjoon/if2.java
+++ src/baekjoon/if2.java
@@ -1,18 +1,56 @@
 package baekjoon;
 
-import java.util.Scanner;
+//import java.util.Scanner; * 정답
 
-public class if2{
+//public class if2{
+//	public static void main(String args[]) {
+//		Scanner scanner = new Scanner(System.in);
+//		
+//		int a = scanner.nextInt();
+//		
+//		if ((a %4==0 && a%100!=0) || a%400 ==0) {
+//			System.out.println("1");
+//		}else {
+//			System.out.println("0");
+//		}
+//	}
+//}
+
+//정답
+import java.util.Scanner; 
+
+public class if2 {
 	public static void main(String args[]) {
 		Scanner scanner = new Scanner(System.in);
 		
 		int a = scanner.nextInt();
+		int b = scanner.nextInt();
 		
-		if ((a %4==0 && a%100!=0) || a%400 ==0) {
+		if (a >0 && b>0) {
 			System.out.println("1");
-		}else {
-			System.out.println("0");
-		}
+		}else if (a>0 && b<0 ) {
+			System.out.println("4");
+		}else if (a <0 && b>0) {
+			System.out.println("2");
+		} else System.out.println("3");
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
