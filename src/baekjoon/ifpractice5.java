4680f196b523ac323134bc5caa8b2fac0b61b9c8
김관민 <@DESKTOP-A5HFT58>
2021-09-29 21:45:38 +0900

    욍?

--- src/baekjoon/ifpractice5.java
+++ src/baekjoon/ifpractice5.java
@@ -1,5 +1,20 @@
 package baekjoon;
 
-public class ifpractice5 {
+	import java.util.Scanner; 
 
-}
+	public class ifpractice5 {
+		public static void main(String args[]) {
+			Scanner scanner = new Scanner(System.in);
+			
+			int a = scanner.nextInt();
+			int b = scanner.nextInt();
+			
+			if (a >0 && b>0) {
+				System.out.println("1");
+			}else if (a>0 && b<0 ) {
+				System.out.println("4");
+			}else if (a <0 && b>0) {
+				System.out.println("2");
+			} else System.out.println("3");
+		}
+	}
