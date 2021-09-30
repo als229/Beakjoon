d111be88c4d98e8f61b7b526ec6a804795cdf46b
als229 <rhksalsdldia@naver.com>
2021-10-01 00:17:17 +0900

    for 문 1번 2번 3번

---
+++ src/baekjoon/forjava.java
@@ -0,0 +1,97 @@
+package baekjoon;
+
+
+
+//1번 정답
+//import java.util.Scanner;
+
+//public class forjava {
+	//
+	//	public static void main(String[] args) {
+	//		Scanner scanner = new Scanner(System.in);
+	//		
+	//		int a = scanner.nextInt();
+	//		
+	//		for(int i = 1; i <10;i++) {
+	//			System.out.println(a +" * "+ i+" = " +a*i);
+	//		}
+	//	}
+	//
+//}
+
+
+
+
+//2번 정답
+//import java.util.Scanner;
+//public class forjava {
+//	public static void main(String[] args) {
+//
+//		Scanner scanner = new Scanner(System.in);
+//
+//		int a = scanner.nextInt();
+//		for (int i = 0; i < a;i++) {
+//			int b = scanner.nextInt();
+//			int c = scanner.nextInt();
+//
+//			System.out.println(b+c);
+//
+//		}
+//
+//	}
+//}
+
+
+//3번 정답
+//import java.util.Scanner;
+//
+//public class forjava {
+//	
+//	public static void main(String[] args) {
+//
+//		Scanner scanner = new Scanner(System.in);
+//		int sum = 0;
+//		
+//		int a = scanner.nextInt();
+//		
+//		for (int i = 1; i <=a;i++) {
+//			
+//			sum += i;
+//
+//		}
+//		System.out.println(sum);
+//
+//	}
+//}
+
+import java.util.Scanner;
+
+public class forjava {
+	
+	public static void main(String[] args) {
+
+		Scanner scanner = new Scanner(System.in);
+		int sum = 0;
+		
+		int a = scanner.nextInt();
+		
+		for (int i = 1; i <=a;i++) {
+			
+			sum += i;
+
+		}
+		System.out.println(sum);
+
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
