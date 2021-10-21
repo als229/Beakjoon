7112e826d9fa294ef98b7310e8f3fcef13aac189
als229 <rhksalsdldia@naver.com>
2021-10-22 01:47:46 +0900

    while 한문제

---
+++ src/baekjoon/While.java
@@ -0,0 +1,23 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+
+//1¹ø ´ä
+public class While {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		while(true){
+			int num1 = sc.nextInt();
+			int num2 = sc.nextInt();
+
+			if( num1 == 0 && num2 == 0) {
+				break;
+			}
+			System.out.println(num1 + num2);
+		}
+	}
+}
