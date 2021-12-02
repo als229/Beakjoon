d23f766e568235d5be3f9ee630db674e4f0036eb
als229 <rhksalsdldia@naver.com>
2021-12-02 21:17:25 +0900

    백준 문자열 한문제...

---
+++ src/baekjoon/BeakJoonString.java
@@ -0,0 +1,17 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class BeakJoonString {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		char s = sc.nextLine().charAt(0);
+		
+		System.out.println((int)s);
+		
+	}
+
+}
